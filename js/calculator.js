14c4c1ed6d2a92ae2df2cf08e7f233a500dd9b9a
SuspiciousApplePie <andreiusita6@gmail.com>
2026-04-15 13:45:13 +0800

    Add subtraction

--- js/calculator.js
+++ js/calculator.js
@@ -4,3 +4,22 @@ export function addition(addend1, addend2) {
   if (isFalsy || isNotANumber) return "Enter a number";
   return addend1 + addend2;
 }
+
+export function subtraction(minuend, subtrahend) {
+  const [isNotANumberMinuend, isNotANumberSubtrahend] = [
+    isNaN(minuend),
+    isNaN(subtrahend),
+  ];
+  const [isFalsyMinuend, isFalsySubtrahend] = [
+    !minuend && minuend !== 0,
+    !subtrahend && subtrahend !== 0,
+  ];
+  if (isFalsyMinuend && isFalsySubtrahend) return "Enter subtraction values.";
+  if (isNotANumberMinuend && isNotANumberSubtrahend)
+    return "Enter valid subtraction values.";
+  if (isNotANumberMinuend) return "Enter a valid minuend.";
+  if (isNotANumberSubtrahend) return "Enter a valid subtrahend.";
+  if (isFalsyMinuend) return "Enter a minuend.";
+  else if (isFalsySubtrahend) return "Enter a subtrahend.";
+  return minuend - subtrahend;
+}
--- js/calculator.test.js
+++ js/calculator.test.js
@@ -1,4 +1,4 @@
-import { addition } from "./calculator";
+import { addition, subtraction } from "./calculator";
 
 test("Addition", () => {
   expect(addition(2, 2)).toBe(4);
@@ -14,4 +14,22 @@ test("Addition", () => {
   expect(addition(console.log, String)).toBe("Enter a number");
   expect(addition("kok", "korokok")).toBe("Enter a number");
   expect(addition(-2, -5)).toBe(-7);
+});
+
+test("Subtraction", () => {
+  expect(subtraction(1, 1)).toBe(0);
+  expect(subtraction(0, 1)).toBe(-1);
+  expect(subtraction(1, 0)).toBe(1);
+  expect(subtraction(0, 0)).toBe(0);
+  expect(subtraction(null, 1)).toBe("Enter a minuend.");
+  expect(subtraction(1, null)).toBe("Enter a subtrahend.");
+  expect(subtraction(undefined, NaN)).toBe("Enter subtraction values.");
+  expect(subtraction("kok", 1)).toBe("Enter a valid minuend.");
+  expect(subtraction(1, "korokok")).toBe("Enter a valid subtrahend.");
+  expect(subtraction("kok", "korokok")).toBe("Enter valid subtraction values.");
+  expect(subtraction(null, null)).toBe("Enter subtraction values.");
+  expect(subtraction(String, console.log)).toBe(
+    "Enter valid subtraction values.",
+  );
+  expect(subtraction(-1, -1)).toBe(0);
 });
